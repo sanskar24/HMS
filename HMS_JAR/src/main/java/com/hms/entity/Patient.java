b75b3214c1aeb305b82f8bf9070f95ae262613a6
deveshbamola <deveshbamola@gmail.com>
2023-07-02 22:49:10 +0530

    Updated Patient.java, Added UMl diagram 1.0

--- HMS_JAR/src/main/java/com/hms/entity/Patient.java
+++ HMS_JAR/src/main/java/com/hms/entity/Patient.java
@@ -1,19 +1,59 @@
 package com.hms.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.HashMap;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Patient {
 
     private int patientId;
 
-    private String patientDisease;
+    private HashMap<String, Integer> recurringAppointment;
 
     private String patientName;
+
+    public Patient(int patientId, String patientName) {
+        this.patientId = patientId;
+        this.patientName = patientName;
+        recurringAppointment = new HashMap<String, Integer>();
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public HashMap<String, Integer> getRecurringAppointment() {
+        return recurringAppointment;
+    }
+
+    public int setRecurringAppointment(String patientDisease, int docId) {
+        if(recurringAppointment.get(patientDisease) == null) {
+            recurringAppointment.put(patientDisease, docId);
+        }
+        else{
+            docId = recurringAppointment.get(patientDisease);
+        }
+        return docId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientId=" + patientId +
+                ", recurringAppointment=" + recurringAppointment +
+                ", patientName='" + patientName + '\'' +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
